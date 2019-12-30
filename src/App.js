a96a10200b1dcd701fbe8a04230699bbbe9b301c
nubelsondev <nubelsondev@hotmail.com>
2019-12-30 12:57:50 +0000

    fead: add Login structure

--- src/App.js
+++ src/App.js
@@ -1,10 +1,26 @@
 import React from 'react'
 import './App.css'
 
+import logo from './assets/logo.svg'
+
 const App = () => {
 	return (
-		<div>
-			<h1>Hello World!</h1>
+		<div className='container'>
+			<img src={logo} alt='AirCnC' />
+
+			<div className='content'>
+				<p>
+					Ofereça <strong>spots</strong> para programadores e encontre{' '}
+					<strong>talentos</strong> para a sua empresa
+				</p>
+
+				<form>
+					<label htmlFor='email'>E-MAIL *</label>
+					<input type='email' id='email' placeholder='Seu e-mail' />
+
+					<button type='submit'>Entrar</button>
+				</form>
+			</div>
 		</div>
 	)
 }
