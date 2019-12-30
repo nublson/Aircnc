f8161c6951a24c4a76df2d766c4a6498e42785ed
nubelsondev <nubelsondev@hotmail.com>
2019-12-30 19:12:11 +0000

    feat: add New page structure

--- src/pages/New/index.js
+++ src/pages/New/index.js
@@ -1,7 +1,76 @@
 import React from 'react'
+import { Formik, Form, Field } from 'formik'
+
+import camera from '../../assets/camera.svg'
+import './styles.scss'
 
 const New = () => {
-	return <h1>New</h1>
+	return (
+		<Formik
+			initialValues={{
+				thumbnail: null,
+				company: '',
+				price: '',
+				techs: ''
+			}}
+			onSubmit={(values, actions) => {
+				console.log(values)
+			}}
+		>
+			{props => (
+				<Form>
+					<label id='thumbnail'>
+						<input
+							type='file'
+							name='thumbnail'
+							onChange={e =>
+								(props.values.thumbnail =
+									e.currentTarget.files[0])
+							}
+						/>
+
+						<img src={camera} alt='Select img' />
+					</label>
+
+					<label htmlFor='company'>
+						EMPRESA <span className='required'>*</span>
+					</label>
+					<Field
+						type=''
+						name='company'
+						id='company'
+						placeholder='Nome da sua empresa...'
+					/>
+
+					<label htmlFor='price'>
+						PREÇO <span className='required'>*</span>{' '}
+						<span>(em branco para GRATUITO)</span>{' '}
+					</label>
+					<Field
+						type=''
+						name='price'
+						id='price'
+						placeholder='Valor Diário...'
+					/>
+
+					<label htmlFor='techs'>
+						TECNOLOGIAS <span className='required'>*</span>{' '}
+						<span>(separadas por vírgula)</span>{' '}
+					</label>
+					<Field
+						type=''
+						name='techs'
+						id='techs'
+						placeholder='Tecnologias usadas...'
+					/>
+
+					<button type='submit' className='btn'>
+						Cadastrar
+					</button>
+				</Form>
+			)}
+		</Formik>
+	)
 }
 
 export default New
